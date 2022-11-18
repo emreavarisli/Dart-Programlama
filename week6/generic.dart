abdf2abbaae2fae13e579ee9500598729a9f7e5a
Hasan Emre AVARİSLİ <81103889+emreavarisli@users.noreply.github.com>
2022-11-18 13:42:26 +0300

    generic

---
+++ week6/generic.dart
@@ -0,0 +1,27 @@
+void main(List<String> args) {
+  final user = User(data: 1);  
+  final adminUser = AdminUser();
+  removeUserAll(adminUser);
+}
+
+void removeUserAll<T extends IAdmin>(T data){
+  data.removeUser();
+}
+
+class User<T> {
+  T data;
+  User({
+    required this.data,
+  });
+}
+
+abstract class IAdmin {
+  void removeUser();
+}
+
+class AdminUser extends IAdmin {
+  @override
+  void removeUser() {
+  }
+}
+
